b8d7f361e2ac24c5ada3d7dc2a90061ad722118e
pk <pk@192.168.1.239>
2019-03-05 13:03:25 +0530

    Multiple Analytics Added and removal of client id and secret

--- BRIGHTCOVESpringOAuth2/src/main/java/com/example/demo/AcccesTokenRequest.java
+++ BRIGHTCOVESpringOAuth2/src/main/java/com/example/demo/AcccesTokenRequest.java
@@ -8,8 +8,8 @@ import org.apache.oltu.oauth2.common.message.types.GrantType;
 
 public class AcccesTokenRequest {
 	public String tokenreqUrl="https://oauth.brightcove.com/v4/access_token";
-	public String CLIENT_ID = "10cb4923-5936-41aa-a7b8-09a125ef0461";
-	public String CLIENT_SECRET ="cyDxmF6lx9AHp6V4FajXKmE4TA7fT4jHkmVZt824hLcPTDspOP1seWvBnMutBLczfd_tYQ_B9QzTfCBrqWcCxA";
+	public String CLIENT_ID = "";
+	public String CLIENT_SECRET ="";
 	public String ACCOUNT_ID = "6005208615001";
 	public String token="";
 	 public void getAccessTOkenMethod() throws Exception{
